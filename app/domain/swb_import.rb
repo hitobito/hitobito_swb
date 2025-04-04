9d02d8d2c0f469a057bbfd6b3f376e8671563bd0
Andreas Maierhofer <a.maierhofer@gmail.com>
2025-04-04 16:53:47 +0200

    Working on info script

--- app/domain/swb_import.rb
+++ app/domain/swb_import.rb
@@ -73,9 +73,9 @@ module SwbImport
   # For mapping specific role types
   ROLE_TYPE_MAPPING = [
     ["", ""],
-    ["Behindertensport", "Aktiv"],
-    ["Center Contact", Group::Center::Kontakt],
-    ["Club Ausbildung", Group::VereinKontakte::Kontakt],
+    ["Behindertensport", "Aktiv"],  # 1 auf SB
+    ["Center Contact", Group::Center::Kontakt], # 11 auf VereinsEbene ( zb. BC Thun)
+    ["Club Ausbildung", Group::VereinKontakte::Kontakt], # 111 auf Vereinsebenen ->
     ["Club Breitensport", Group::VereinKontakte::Kontakt],
     ["Club Ethik", Group::VereinKontakte::Kontakt],
     ["Club Events/Turniere", Group::VereinMitglieder::Turnierorganisator],
@@ -92,6 +92,7 @@ module SwbImport
     ["Club Präsident", Group::VereinVorstand::Praesident],
     ["Club Schiedsrichter", Group::VereinKontakte::Kontakt],
     ["Club Sport Integrity", Group::VereinKontakte::Kontakt],
+
     ["Club Vize-Präsident", Group::VereinVorstand::Vizepraesident],
     ["Clubtrainer", Group::VereinMitglieder::Clubtrainer],
     # ["Ehemalige ZV-Mitglieder", ""],
@@ -130,4 +131,54 @@ module SwbImport
   ].to_h
 
   require_relative "swb_import/entity"
+
+  Mitglied = Data.define(:role, :type_name, :groupname)
+
+  class InfoGenerator
+    def initialize(key = "mitglieder-short")
+      @csv = SwbImport::Csv.new(key).csv
+      @mitglieder = read_mitglieder
+    end
+
+    def write
+      File.write("ts_hitobito_roles.csv", generate)
+    end
+
+    def generate
+      by_role = @mitglieder.group_by(&:role).sort_by { |k, v| v.size }.reverse
+      group_names = @mitglieder.map(&:groupname).tally.sort_by(&:second).reverse.map(&:first)
+      binding.pry
+      @headers = ["TS Rolle", "TS Typ", "Total", "Rolle Hitobito", "Total Gruppen", *group_names]
+      rows = CSV.generate do |csv|
+        csv << @headers
+        by_role.each do |role, mitglieder|
+          if mitglieder.map(&:type_name).empty?
+            csv << add_row(role, mitglieder, ROLE_MAPPING[role])
+          else
+            mitglieder.group_by(&:type_name).each do |type_name, mitglieder|
+              csv << add_row(role, mitglieder, type_name:)
+            end
+          end
+        end
+      end
+    end
+
+    def add_row(role, mitglieder, type_name: nil)
+      hitobito_role = type_name.present? ? SPIELER_LIZENZ_MAPPING[type_name] : ROLE_TYPE_MAPPING[role]
+      group_names = mitglieder.map(&:groupname)
+      [role, hitobito_role, mitglieder.size, hitobito_role, group_names.uniq.count].tap do |row|
+        group_names.tally.each do |group_name, count|
+          pos = @headers.index(group_name)
+          row[pos] = count
+        end
+      end
+    end
+
+    def read_mitglieder
+      @csv.map do |r|
+        attrs = r.to_h.transform_keys { |k| k.underscore.to_sym }.slice(*Mitglied.members)
+        Mitglied.new(**attrs)
+      end
+    end
+  end
 end
