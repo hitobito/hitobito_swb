a1d65bb207446b211b2f63621c7c8dd1fdd271e0
Niklas <68543592+njaeggi@users.noreply.github.com>
2025-05-27 17:17:52 +0200

    Validate person holds no more  than one player role per group  (#35)

---
+++ app/models/role/player_role.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2025, Swiss Badminton. This file is part of
+#  hitobito_swb and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_swb.
+
+class Role::PlayerRole < ::Role
+  validate :only_one_player_role_per_group
+
+  private
+
+  def only_one_player_role_per_group
+    errors.add(:person, :already_player_in_group) if (person.roles.where(group:) - [self]).any?
+  end
+end
---
+++ spec/models/role/player_role_spec.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2025, Swiss Badminton. This file is part of
+#  hitobito_swb and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_swb.
+
+require "spec_helper"
+
+describe Role::PlayerRole do
+  let(:group) { groups(:brb_spieler) }
+  let(:person) { people(:admin) }
+
+  subject(:role) { Group::RegionSpieler::Aktivmitglied.build(group:, person:) }
+
+  describe "::validations" do
+    describe "asserting single player role" do
+      it "is invalid if person has any other role in that group" do
+        Fabricate(Group::RegionSpieler::Lizenz.sti_name, group:, person:)
+        expect(role).not_to be_valid
+        expect(role.errors.full_messages).to eq ["Person hat bereits eine Spielerrolle in dieser Gruppe"]
+      end
+
+      it "is valid if player role exists in a different group" do
+        Fabricate(Group::RegionSpieler::Lizenz.sti_name, group: groups(:bvn_spieler), person:)
+        expect(role).to be_valid
+      end
+
+      it "can still update that single player role" do
+        role.save!
+        expect(role.update!(end_on: 3.days.from_now)).to eq true
+      end
+    end
+  end
+end
