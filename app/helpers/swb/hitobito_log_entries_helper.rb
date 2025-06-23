8ef1546b27ba223a7232a217f3d3637e73b3de06
Andreas Maierhofer <a.maierhofer@gmail.com>
2025-06-23 09:01:19 +0200

    Show person instead of role as hitobito_log_entry subject

---
+++ app/helpers/swb/hitobito_log_entries_helper.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2012-2025, Swiss Badminton. This file is part of
+#  hitobito_swb and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_swb.
+
+module Swb::HitobitoLogEntriesHelper
+  def format_hitobito_log_entry_subject(e)
+    case e.subject
+    when Role then link_to(e.subject.person, e.subject.person)
+    else format_assoc(e, association(e, :subject, :belongs_to))
+    end
+  end
+end
