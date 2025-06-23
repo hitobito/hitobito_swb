3c88061fb6b82bf5e5314d7aed5a6c3de0303aa3
Andreas Maierhofer <a.maierhofer@gmail.com>
2025-06-23 09:01:19 +0200

    Show person instead of role as hitobito_log_entry subject

---
+++ spec/views/hitobito_log_entries/_list.html.haml_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2012-2025, Swiss Badminton. This file is part of
+#  hitobito_swb and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_swb.
+
+require "spec_helper"
+
+describe "hitobito_log_entries/_list.html.haml" do
+  let(:dom) {
+    render
+    Capybara::Node::Simple.new(@rendered)
+  }
+
+  it "links to person for role subject" do
+    entries = Kaminari.paginate_array([
+      Fabricate(:ts_log, subject: roles(:leader))
+    ])
+    allow(entries).to receive_messages(total_pages: 1, current_page: 1)
+    allow(view).to receive_messages(model_class: HitobitoLogEntry, category_param: "", entries: entries)
+    expect(dom).to have_link("A Leader", href: "/de/people/#{people(:leader).id}")
+  end
+end
