5dc54395574f72ca3e9fc9e15b5f1945dbf23d6b
Andreas Maierhofer <a.maierhofer@gmail.com>
2025-09-12 10:35:18 +0200

    Map nationality from ioc to iso 3166-2 (#121)

---
+++ db/migrate/20250912082729_update_people_nationality.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2012-2025, Swiss Badminton. This file is part of
+#  hitobito_swb and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_swb.
+
+class UpdatePeopleNationality < ActiveRecord::Migration[7.1]
+  def up
+    iso3166_ioc_country_mapping.each do |iso, ico|
+      next if iso.blank?
+
+      execute "UPDATE people SET nationality = '#{iso}' WHERE nationality = '#{ioc}'"
+    end
+  end
+
+  private
+
+  def iso3166_ioc_country_mapping
+    Person
+      .pluck('distinct(nationality)')
+      .compact
+      .select { |c| c == Country.new(c).name }
+      .map { |c| [SwbImport::Parser.new(nil, nil).send(:parse_country, c), c] }
+  end
+end
