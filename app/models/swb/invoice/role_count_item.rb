a55e51b737268ce02ff90724e9bf8b27744aaad6
carlobeltrame <carlo@beltra.me>
2026-01-22 17:32:37 +0100

    Remove broken specs for features which will be reimplemented in the core (hitobito/hitobito#3750)

--- app/models/swb/invoice/role_count_item.rb
+++ app/models/swb/invoice/role_count_item.rb
@@ -10,32 +10,11 @@ module Swb::Invoice::RoleCountItem
 
   prepended do
     include Invoices::UnbilledRolesJoin
-
-    attr_accessor :roles
-
-    has_many :billed_models,
-      inverse_of: :invoice_item,
-      foreign_key: :invoice_item_id,
-      dependent: :delete_all
-
-    after_create :create_billed_models
   end
 
   private
 
   def base_scope
     with_unbilled_roles(super)
-  end
-
-  def create_billed_models
-    rows = roles.map do |role|
-      {
-        invoice_item_id: id,
-        billing_period_id: BillingPeriod.active.id,
-        model_id: role.id,
-        model_type: Role.sti_name
-      }
-    end
-    BilledModel.insert_all(rows)
   end
 end
