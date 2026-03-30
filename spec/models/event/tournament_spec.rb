558bdc351430b44c975cd82cb6721756c001eeea
Niklas Jäggi <jaeggi@puzzle.ch>
2026-03-30 16:55:54 +0200

    Remove global event question seeds (hitobito/hitobito_sac_cas#2322)

--- spec/models/event/tournament_spec.rb
+++ spec/models/event/tournament_spec.rb
@@ -33,36 +33,4 @@ describe Event::Tournament do
       :minimum_participants
     ]
   end
-
-  describe "#init_questions" do
-    before(:all) do
-      SeedFu.quiet = true
-      load HitobitoSwb::Wagon.root.join("db", "seeds", "event_questions.rb").to_s
-    end
-
-    let(:tournament) { Fabricate.build(:tournament).tap(&:init_questions).tap(&:save!) }
-    let(:question) { tournament.application_questions.find(&:multiple_choices?) }
-
-    it "builds 5 application questions" do
-      expect(tournament.application_questions).to have(5).item
-    end
-
-    choices = ["HE / SM", "DE / SD", "HD / DM", "DD / DD", "MX / DX"].join(", ")
-
-    questions = [
-      ["de", "In welchen Disziplinen tritts du an?", choices],
-      ["fr", "Dans quelles disciplines concourrez-vous?", choices],
-      ["it", "In quali discipline gareggia?", choices],
-      ["en", "In which disciplines do you compete?", choices]
-    ]
-
-    questions.each do |locale, text, choices|
-      it "has expected question '#{text}' and choices for #{locale}" do
-        expect(I18n.with_locale(locale) { question.question }).to eq text
-        expect(I18n.with_locale(locale) { question.choices }).to eq choices.to_s
-        expect(question.disclosure).to eq "required"
-        expect(question.multiple_choices).to eq true
-      end
-    end
-  end
 end
